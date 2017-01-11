1f36c0ba6dfe177b4c2063c96bd2035537862056
Célian JONATHAN <celian.jonathan@epita.fr>
2017-01-11 01:06:46 +0100

    add u-boot start compil script

---
+++ src/u-boot_compile.sh
@@ -0,0 +1,22 @@
+# U-Boot compile Script for nanopi m1
+
+
+### Installer de la tool-chain de cross compilation
+
+sudo apt-get install crossbuild-essential-armhf
+export PATH="${PATH}:/usr/bin"
+
+### Récuperation de u-boot
+
+git clone http://git.denx.de/u-boot.git
+cd u-boot/
+git checkout a705ebc81b7f91bbd0ef7c634284208342901149
+
+### Génération du .config et compilation
+
+make ARCH=arm CROSS_COMPILE="arm-linux-gnueabihf-" orangepi_one_defconfig
+make ARCH=arm CROSS_COMPILE="arm-linux-gnueabihf-"
+
+### On récupère le fichier a dd
+
+#### FIXME
